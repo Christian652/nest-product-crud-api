90534ab4acd4bfb6e5d6b7153b50a6736408409a
Christian652 <christianferreira652@gmail.com>
2021-06-03 12:27:09 -0300

    product seeder , and pagination added

--- src/seeder.ts
+++ src/seeder.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/orm'
 import { UserSeeder } from "./user/user.seeder";
 import { UserRepository } from "./user/user.repository";
+import { ProductRepository } from "./product/product.repository";
+import { ProductSeeder } from "./product/product.seeder";
 
 seeder({
   imports: [ 
     TypeOrmModule.forRoot(configService.getTypeOrmData()),
-    TypeOrmModule.forFeature([UserRepository]),
+    TypeOrmModule.forFeature([UserRepository, ProductRepository]),
   ],
-}).run([UserSeeder]);
+}).run([UserSeeder, ProductSeeder]);
